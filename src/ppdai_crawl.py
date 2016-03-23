72f997e190776a1f709916b47976961165f2825f
531671430@qq.com <531671430@qq.com>
2016-03-24 00:48:30 +0800

    modified:   ppdai_crawl.py

--- src/ppdai_crawl.py
+++ src/ppdai_crawl.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import urllib2
 import socket
 import requests
-import re
 
 socket.setdefaulttimeout(10)
 
@@ -23,6 +21,7 @@ request_params = {'Connection': 'keep-alive',
                   }
 
 
+# 用于去除重复的地址
 def unique(items):
     keep = []
     for item in items:
@@ -30,19 +29,11 @@ def unique(items):
             keep.append(item)
     return keep
 
-
+# 发送链接请求
 def do_request(url):
-    # print url
     while True:
         try:
-            # req = urllib2.Request(url)
-            # req.add_header('User-Agent',
-            #                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36')
-            # res = urllib2.urlopen(req)
-            # return res.read()
-            # r = requests.get(url, request_params)
             r = requests.get(url, request_params)
-            # print r.status_code
             if r.status_code != 200:
                 print ("Http request error: " + str(r.status_code) + " ---- " + url)
                 return ""
@@ -131,7 +122,6 @@ def parse_load_single_page(url, info):
 
     # 借款进度
     div = soup.find('div', 'part clearfix')
-    sub_div = div.find('div', 'item w260')
     ret_info.append(div.text)
 
     # 投标数
@@ -146,68 +136,23 @@ def parse_load_single_page(url, info):
     return ret_info
 
 
+# 写入指定文件
 def output_to_file(file, info):
     for item in info:
         file.write(item.encode('utf-8').replace('\n', '').replace('\r', '').replace(' ', '') + ", ")
     file.write('\n')
 
-
 def main():
-    # Open data file in append mode
-    # if len(sys.argv) < 2:
-    # print ("Please specify output file name")
-    # return
-    # file_name = sys.argv[1]
-    file_name = "res.txt"
-    data_file = open(file_name, 'a')
-    user_url_list = []
-
-    s_len = len(start_url_list)
-    for i in range(s_len):
-        start_url = start_url_list[i]
-        for page in range(page_range[i][0], page_range[i][1]):
-            # print page
-            page_url = start_url + "_s0_p" + str(page)
-            ret_user = parse_load_list_page(page_url)
-            # print 'ret_user:', ret_user[0]
-            user_url_list = user_url_list + ret_user
-            # print ('Parsing page: ' + page_url + '......')
-            # res = parse_load_list_page(page_url)
-            # if len(res) == 0:
-            # 	break
-            # for item in res:
-            # 	info = parse_load_single_page(item)
-            # 	if info[9].find('100%') != -1:
-            # 		output_to_file(data_file, info)
-            # 	handled_page_url.append(item)
-            # print ('Finish page: ' + page_url)
-            # page = page + 1
-
-    # Remove duplicated user urls
-    user_url_list = unique(user_url_list)
-
-    out = open('list.txt', 'a')
-
-    for item in user_url_list:
-        #print ('Parsing user: ' + item)
-        info_list = parse_user_page(item)
-        for info in info_list:
-            output_to_file(data_file, info)
-        print (str(len(info_list)) + " items added")
-
-
-def add1():
     socket.setdefaulttimeout(10)
     data_file = open('res13.txt', 'a')
 
     # get user_url_list from links
     links = ['http://www.ppdai.com/list/%d' % i for i in range(4627000,4629000)]
+
     for url in links:
         print url
-        # link = get_user_id(url)
         try:
             r = requests.get(url)
-            # print r.status_code
             if r.status_code != 200:
                 print ("Http request error: " + str(r.status_code) + " ---- " + url)
                 continue
@@ -216,112 +161,17 @@ def add1():
             a = soup.find(name='input', id='usernameHidden')
             item = 'http://www.ppdai.com/user/' + a['value']
 
-            # print ('Parsing user: ' + item)
             info_list = parse_user_page(item)
+
             for info in info_list:
                 output_to_file(data_file, info)
+
             print (str(len(info_list)) + " items added")
-            # return re.find(pattern, r.content)
+
         except:
             print 'error'
             pass
-
-def add_mutil(_from,_to,file_name,thread_id):
-    socket.setdefaulttimeout(10)
-    data_file = open(file_name, 'a')
-
-    print thread_id
-    # get user_url_list from links
-    links = ['http://www.ppdai.com/list/%d' % i for i in range(_from,_to)]
-    for url in links:
-        print url
-        # link = get_user_id(url)
-        try:
-            r = requests.get(url)
-            # print r.status_code
-            if r.status_code != 200:
-                print ("Http request error: " + str(r.status_code) + " ---- " + url)
-                continue
-
-            soup = BeautifulSoup(r.content, "lxml")
-            a = soup.find(name='input', id='usernameHidden')
-            item = 'http://www.ppdai.com/user/' + a['value']
-
-            # print ('Parsing user: ' + item)
-            info_list = parse_user_page(item)
-            for info in info_list:
-                output_to_file(data_file, info)
-            print (str(len(info_list)) + " items added")
-            # return re.find(pattern, r.content)
-        except:
-            print 'error'
-            pass
-
-import threading
-from multiprocessing import Process
-
-# mutil processes
-def mutil_proc():
-    #thread.start_new_thread(add_mutil(self, 4301006, 43020000, 'res6.txt'))
-    #thread.start_new_thread(add_mutil(self, 4302001, 43030000, 'res7.txt'))
-    #thread.start_new_thread(add_mutil(self, 4303001, 43040000, 'res7.txt'))
-    #t1 = thread.start_new_thread(add_mutil,(43030001, 43030013, 'restest1.txt'))
-    #t2 = thread.start_new_thread(add_mutil,(43040001, 43040013, 'restest2.txt'))
-
-    # p1 = Process(target=add_mutil,args=(4540001, 4541000, 'res2.txt'))
-    # p2 = Process(target=add_mutil,args=(4541001, 4545000, 'res3.txt'))
-    # p3 = Process(target=add_mutil,args=(4545001, 4547000, 'res4.txt'))
-    p4 = Process(target=add_mutil,args=(3548101, 3549999, 'res5.txt'))
-    p5 = Process(target=add_mutil,args=(3030452, 3132000, 'res6.txt'))
-    p6 = Process(target=add_mutil,args=(3132101, 3133000, 'res7.txt'))
-    p7 = Process(target=add_mutil,args=(3133101, 3134000, 'res8.txt'))
-    p8 = Process(target=add_mutil,args=(3134101, 3135000, 'res9.txt'))
-
-    #p1.start()
-    #p2.start()
-    #p3.start()
-    p4.start()
-    p5.start()
-    p6.start()
-    p7.start()
-    p8.start()
-
-    #p1.join()
-    #p2.join()
-    #p3.join()
-    p4.join()
-    p5.join()
-    p6.join()
-    p7.join()
-    p8.join()
-
-    print "Exiting Main procecss"
-
-# mutil threads
-def mutil_t():
-    t1 = threading.Thread(target=add_mutil, args=(3450361,3541000,'res5.txt', 't1'))
-    t2 = threading.Thread(target=add_mutil, args=(3451422,3542000,'res6.txt', 't2'))
-    t3 = threading.Thread(target=add_mutil, args=(3453211,3544000,'res7.txt', 't3'))
-    t4 = threading.Thread(target=add_mutil, args=(3454511,3545000,'res8.txt', 't4'))
-    t5 = threading.Thread(target=add_mutil, args=(3455761,3549000,'res9.txt', 't5'))
-
-    t = [t1,t2,t3,t4,t5]
-    for i in t:
-        i.start()
-
-    for i in t:
-        i.join()
-        print '--------done'
 
 if __name__ == '__main__':
-    # file_name = "res.txt"
-    # data_file = open(file_name, 'w')
-    # info_list = parse_user_page('http://www.ppdai.com/user/shiyang696')
-    # for info in info_list:
-    #     output_to_file(data_file, info)
-    # print (str(len(info_list)) + " items added")
-    # main()
-    # add1()
-    # mutil_proc()
-    # mutil_t()
-    add1()
+    main()
+
